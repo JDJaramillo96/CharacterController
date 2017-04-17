0b16f0ae691d23324b42e3bca2a233f624074f4a
xoakzxz <juandavid_ja@hotmail.com>
2017-04-16 23:07:52 -0500

    Character Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    private float scrollVelocity;
+
+    [Header("Orbit Settings")]
+
+    [SerializeField]
+    private float orbitVelocity;
+    [SerializeField]
+    private Transform target;
+
+    //Cached components
+    private new Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            ZoomIn();
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            ZoomOut();
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Orbit(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            Orbit(Vector3.down);
+        }
+    }
+
+    private void ZoomIn()
+    {
+        camera.transform.position += transform.forward * scrollVelocity;
+    }
+
+    private void ZoomOut()
+    {
+        camera.transform.position -= transform.forward * scrollVelocity;
+    }
+
+    private void Orbit(Vector3 axis)
+    {
+        transform.RotateAround(target.position, axis, orbitVelocity);
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    [SerializeField]
+    private float velocity;
+    [SerializeField]
+    private Rigidbody character;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            character.AddForce(transform.forward * velocity);
+        }
+    }
+}
